b2facc026a20e0d3d8ef501cfbace34e4ef3150f
niusmallnan <niusmallnan@gmail.com>
2018-07-06 18:22:15 +0800

    Update clean script

--- deploy/longhorn/cleanup.sh
+++ deploy/longhorn/cleanup.sh
@@ -6,5 +6,9 @@ kubectl -n longhorn-system delete volumes.longhorn.rancher.io --all
 kubectl -n longhorn-system delete engineimages.longhorn.rancher.io --all
 kubectl -n longhorn-system delete nodes.longhorn.rancher.io --all
 
-kubectl delete -Rf longhorn.yaml
+# clean csi
+kubectl -n longhorn-system delete  sts --all
+kubectl -n longhorn-system delete  ds --all
 
+
+kubectl delete -Rf longhorn.yaml
